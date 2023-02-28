1ef9f1ff2f8c47a73288ee915ea24f00dc35f62c
pgrote <patrick.grote@dmxcontrol-projects.org>
2023-02-28 01:48:24 +0100

    Add missing Shape-Types and Side-Lengths

--- NanoleafAPI/API/JSON-Objects/PanelLayout/PanelPosition.cs
+++ NanoleafAPI/API/JSON-Objects/PanelLayout/PanelPosition.cs
@@ -15,7 +15,14 @@ namespace NanoleafAPI
             Hexagon_Shapes = 7,
             Triangle_Shapes = 8,
             MiniTriangle_Shapes = 9,
-            ShapesController = 12
+            ShapesController = 12,
+            ElementsHexagons = 14,
+            ElementsHexagonsCorner = 15,
+            LinesConnector = 16,
+            LightLines = 17,
+            LightLinesSingleZone = 18,
+            ControllerCap = 19,
+            PowerConnector = 20
         }
         [JsonProperty("panelId")]
         public int PanelId { get; set; }
@@ -37,7 +44,7 @@ namespace NanoleafAPI
             return $"PanelID: {PanelId} X: {X} Y: {Y} Orentation: {Orientation} Shape: {ShapeType}";
         }
 
-        public int SideLength
+        public double SideLength
         {
             get
             {
@@ -46,17 +53,28 @@ namespace NanoleafAPI
                     case EShapeType.Triangle:
                         return 150;
                     case EShapeType.Rhythm:
-                    case EShapeType.ShapesController:
                         return 0;
                     case EShapeType.Square:
                     case EShapeType.ContolSquarePassive:
                     case EShapeType.ControlSquarePrimary:
                         return 100;
                     case EShapeType.Hexagon_Shapes:
+                    case EShapeType.ShapesController:
                     case EShapeType.MiniTriangle_Shapes:
                         return 67;
                     case EShapeType.Triangle_Shapes:
+                    case EShapeType.ElementsHexagons:
                         return 134;
+                    case EShapeType.ElementsHexagonsCorner:
+                        return 33.5; //58
+                    case EShapeType.LightLines:
+                        return 154;
+                    case EShapeType.LightLinesSingleZone:
+                        return 77;
+                    case EShapeType.LinesConnector:
+                    case EShapeType.ControllerCap:
+                    case EShapeType.PowerConnector:
+                        return 11;
                 }
                 return 0;
             }
